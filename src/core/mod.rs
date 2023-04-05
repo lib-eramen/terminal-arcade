5daf87f570052cfd96e2e64362ee8e743de1a111
Ramen Developer <ramendev2009@gmail.com>
2023-04-05 10:42:54 +0700

    FEATURE: Core module instantiated

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,1 +1,21 @@
+//! # The `core` module
+//!
+//! This module contains all of the inner workings and mechanisms for Terminal
+//! Arcade - including its UI, text, game loading, and custom game engine.
+//!
+//! Okay, "custom game engine" sounds a bit overblown. It's just an event
+//! handling trait.
+//!
+//! To get started, take a look at the [`TerminalArcade`] struct, the struct
+//! that all mechanics in this crate is based on.
 
+/// The core struct to all inner workings in Terminal Arcade.
+/// For now, this struct is a unit struct.
+pub struct TerminalArcade;
+
+impl TerminalArcade {
+	/// The function to be called when Terminal Arcade starts up.
+	pub fn startup() {
+		todo!()
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+//! # Terminal Arcade
+//!
+//! Terminal Arcade is an arcade machine
+//! replica-concept-reinvention-do-it-myself thingymajingy of the arcade
+//! machine! The preceding sentence should have already given a solid indication
+//! of the quality of this crate.
+//!
+//! This crate contains an interface for extending and building more games, as
+//! well as a (hopefully) lot other pre-built games as well.
+
+#![deny(unused_must_use, unused_imports, rust_2018_idioms)]
+#![warn(clippy::all, clippy::pedantic, missing_docs)]
+
 /// Entry point of a program! What, you expected a `main` function to be
 /// documented well?
 fn main() {
