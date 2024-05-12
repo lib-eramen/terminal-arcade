729774a5d98987203202707f394adba6f115f530
Viet Trong Duong <ramendev2009@gmail.com>
2024-05-12 09:14:00 +0000

    clear things from minesweeper setup screen

--- src/ui/screens/games/minesweeper/board_setup.rs
+++ src/ui/screens/games/minesweeper/board_setup.rs
@@ -29,38 +29,14 @@ use crate::ui::{
 #[derive(new, Clone)]
 pub struct MinesweeperSetupScreen;
 
-impl MinesweeperSetupScreen {
-	/// Returns the layout for the Minesweeper board setup screen.
-	#[must_use]
-	fn board_setup_layout() -> Layout {
-		let info_panel_height = 3 + 4;
-		let constraints = vec![Constraint::Max(info_panel_height), Constraint::Min(0)];
-		Layout::default()
-			.direction(Direction::Vertical)
-			.vertical_margin(1)
-			.horizontal_margin(1)
-			.constraints(constraints)
-	}
-}
-
 impl Screen for MinesweeperSetupScreen {
 	fn initial_state(&self) -> ScreenState {
 		ScreenState::new("Mine your field!", ScreenKind::Normal, None)
 	}
 
-	fn render_screen(&mut self, frame: &mut Frame<'_>, _state: &ScreenState) {
-		let size = frame.size();
-		let chunks = Self::board_setup_layout().split(size);
+	fn render_screen(&mut self, _frame: &mut Frame<'_>, _state: &ScreenState) {}
 
-		frame.render_widget(titled_ui_block("Controls"), chunks[0]);
-	}
-
-	fn event(&mut self, event: &Event, state: &mut ScreenState) -> anyhow::Result<()> {
-		if let Event::Key(key) = event {
-			if let KeyCode::Enter = key.code {
-				state.open_status = OpenStatus::Closed;
-			}
-		}
+	fn event(&mut self, _event: &Event, _state: &mut ScreenState) -> anyhow::Result<()> {
 		Ok(())
 	}
 }
