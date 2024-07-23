d79ee6677ae85d4260556f961c36c92398830512
Viet Trong Duong <liberamen19@gmail.com>
2024-07-23 01:45:17 +0000

    reset to an empty cargo project... yet again

--- src/log.rs
+++
@@ -1,8 +0,0 @@
-//! Utilities for setting up logging.
-
-use color_eyre::eyre::Result;
-
-/// Initializes logging utilities.
-pub fn init_logging() -> Result<()> {
-	todo!()
-}
--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,6 @@
 //! Expect this to be a work-in-progress always! New games and features and
 //! to-be-debugged spaghetti code guaranteed.
 
-use color_eyre::eyre::Result;
-
-use crate::{
-	log::init_logging,
-	panic::init_panic_handling,
-};
-
-pub mod log;
-pub mod panic;
-
-fn main() -> Result<()> {
-	init_panic_handling()?;
-	init_logging()?;
-	Ok(())
+fn main() {
+	println!("Hello, world!");
 }
